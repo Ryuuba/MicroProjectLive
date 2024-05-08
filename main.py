70a5cdca0ddeaeacbb365d7680a82d4335bfb60f
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-08 13:47:04 -0600

    Digital clock test is OK

---
+++ main.py
@@ -0,0 +1,12 @@
+from test import clock_test
+
+debug = True
+
+def main() -> None:
+    pass
+
+if __name__ == '__main__':
+    if debug:
+        clock_test()
+    else:
+        main()
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from digital_clock import DigitalClock
 from utime import sleep
 
 def clock_test() -> None:
-    clock = DigitalClock()
+    clock = DigitalClock(23, 59, 40)
     while True:
         h, m, s = clock.get_time()
         time = f'{h:02} : {m:02} : {s:02}'
