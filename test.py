5d45b3778261124ed73275837b0632e96f70feb8
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-08 13:31:46 -0600

    Code digital clock test

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 """This module holds functions to test the functionality of this project
 """
+from digital_clock import DigitalClock
+from utime import sleep
 
 def clock_test() -> None:
-    pass
+    clock = DigitalClock()
+    while True:
+        h, m, s = clock.get_time()
+        time = f'{h:02} : {m:02} : {s:02}'
+        print(time)
+        sleep(1)
+        clock.increment()
